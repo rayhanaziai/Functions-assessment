ff6fc2a32ea46b7f41bada9c8a9dd243b3e300e1
Rayhana Ziai <rayhana.z@hotmail.com>
2017-01-09 04:02:48 +0000

    tests passed

--- practice.py
+++ practice.py
@@ -72,30 +72,76 @@ PART TWO:
 #    prints "Hello World".
 
 
+def hello_world():
+    """prints Hello World"""
+
+    print "Hello World"
+
+
 # 2. Write a function called 'say_hi' that takes a name as a string and
 #    prints "Hi" followed by the name.
+def say_hi(name):
+    """ prints Hi along with the input name."""
+
+    print "Hi " + name
 
 
 # 3. Write a function called 'print_product' that takes two integers and
 #    multiplies them together. Print the result.
+def print_product(number1, number2):
+    """prints the product of the two input integers"""
 
+    result = number1 * number2
+    print result
 
 # 4. Write a function called 'repeat_string' that takes a string and an integer
 #    and prints the string that many times
 
+
+def repeat_string(word, number):
+    """prints the string the same number of times as the input number"""
+
+    result = ""
+    for i in range(number):
+        result = result + word
+    print result
 
 # 5. Write a function called 'print_sign' that takes an integer and prints
 #    "Higher than 0" if higher than zero and "Lower than 0" if lower than zero.
 #    If the integer is zero, print "Zero".
 
 
+def print_sign(number):
+    """prints whether the number is higher than zero, lower than zero, or zero"""
+
+    if number == 0:
+        print "Zero"
+    elif number > 0:
+        print "Higher than 0"
+    elif number < 0:
+        print "Lower than 0"
 # 6. Write a function called 'is_divisible_by_three' that takes an integer and
 #    returns a boolean (True or False), depending on whether the number is
 #    evenly divisible by 3.
 
 
+def is_divisible_by_three(number):
+    """ returns true if number is divisible by 3 and false otherwise"""
+
+    if number % 3 == 0:
+        return True
+    else:
+        return False
+
+
 # 7. Write a function called 'num_spaces' that takes a sentence as one string
 #    and returns the number of spaces.
+def num_spaces(sentence):
+    """returns the number of spaces in a sentence"""
+
+    lst = sentence.split(" ")
+    result = len(lst) - 1
+    return result
 
 
 # 8. Write a function called 'total_meal_price' that can be passed a meal price
@@ -103,6 +149,11 @@ PART TWO:
 #    (price + price * tip). **However:** passing in the tip percentage should
 #    be optional; if not given, it should default to 15%.
 
+def total_meal_price(meal_price, tip=0.15):
+    """returns the total meal price when tip is also taken into account"""
+
+    tip_amount = tip * meal_price
+    return tip_amount + meal_price
 
 # 9. Write a function called 'sign_and_parity' that takes an integer as an
 #    argument and returns two pieces of information as strings --- "Positive"
@@ -114,6 +165,25 @@ PART TWO:
 #    it's even or odd). Print sign and parity.
 
 
+def sign_and_parity(number):
+    """returns both a numbers sign and its parity"""
+
+    result = [None, None]
+    if number > 0:
+        result[1] = "Positive"
+    elif number < 0:
+        result[1] = "Negative"
+    if number % 2 == 0:
+        result[0] = "Even"
+    else:
+        result[0] = "Odd"
+    return result
+
+parity, sign = sign_and_parity(65)
+print sign
+print parity
+
+
 ###############################################################################
 
 # PART TWO
@@ -121,6 +191,13 @@ PART TWO:
 # 1. Write a function that takes a name and a job title as parameters, making
 #    it so the job title defaults to "Engineer" if a job title is not passed
 #    in. Return the person's title and name in one string.
+
+
+def full_title(name, job="Engineer"):
+    """ returns a persons job title and then name in pne string"""
+
+    return job + " " + name
+
 
 # 2. Given a recipient name & job title and a sender name, print the following
 #    letter:
@@ -131,6 +208,10 @@ PART TWO:
 #    Use the function from #1 to construct the full title for the letter's
 #    greeting.
 
+def write_letter(name, job, sender_name):
+
+    full_title_recipient_name = full_title(name, job)
+    print "Dear %s, I think you are amazing! Sincerely, %s" % (full_title_recipient_name, sender_name)
 
 ###############################################################################
 
